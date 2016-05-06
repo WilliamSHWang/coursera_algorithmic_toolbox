4afc89ba82c3768845d6b6d88a741f6b0d060e52
Shanhe Wang <shanhewang@gmail.com>
2016-05-06 18:32:47 +0000

    init

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	g++ gcd.cpp -o gcd.out
+	g++ fib.cc -o fib.out
+	g++ fibonacci_last_digit.cpp -o fibonacci_last_digit.out
---
+++ fibonacci_last_digit.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+int get_fibonacci_last_digit(long long n) {
+  //write your code here
+  if (n <= 1){
+      return n;
+  }else{
+      std::vector<int> f;
+      f.push_back(0);
+      f.push_back(1);
+      for(int i=2; i<=n; i++){
+        f.push_back((f[i-1] + f[i-2]) % 10);
+      }
+      return f.back();
+  }
+}
+
+int main() {
+  long long n = 0;
+  std::cin >> n;
+  int c = get_fibonacci_last_digit(n);
+  std::cout << c << '\n';
+  
+    return 0;
+}
