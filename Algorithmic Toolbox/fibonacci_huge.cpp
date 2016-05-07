8dd9cfa9110661d681fc5867dbb6a244a4c08008
William <shanhewang@gmail.com>
2016-05-07 02:12:10 +0000

    Modified the advance problem

--- Algorithmic Toolbox/fibonacci_huge.cpp
+++ Algorithmic Toolbox/fibonacci_huge.cpp
@@ -6,15 +6,20 @@ long long get_fibonaccihuge(long long n, long long m) {
   //write your code here
   
   if (n <= 1){
-      return n;
+    return n;
   }else{
-      std::vector<int> f;
-      f.push_back(0);
-      f.push_back(1);
-      for(int i=2; i<=n; i++){
-        f.push_back((f[i-1] + f[i-2]) % m);
-      }
-      return f.back();
+    int period = 0;
+    std::vector<int> f;
+    f.push_back(0);
+    f.push_back(1);
+    for(long long i=2; i<=n; i++){
+      period++;
+      f.push_back((f[i-1] + f[i-2]) % m);
+      if(f.back() == 1 && f[f.size()-2] == 0)
+        break;
+    }
+    int index = n % period;
+    return f[index];
   }
   
   
--- Algorithmic Toolbox/fibonacci_last_digit.cpp
+++ Algorithmic Toolbox/fibonacci_last_digit.cpp
@@ -10,7 +10,7 @@ int get_fibonacci_last_digit(long long n) {
       std::vector<int> f;
       f.push_back(0);
       f.push_back(1);
-      for(int i=2; i<=n; i++){
+      for(long long i=2; i<=n; i++){
         f.push_back((f[i-1] + f[i-2]) % 10);
       }
       return f.back();
