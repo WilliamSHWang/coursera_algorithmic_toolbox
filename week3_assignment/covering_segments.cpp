e1c358104a9aa9580a7f4efe06b597c1b4a97ffe
William <shanhewang@gmail.com>
2016-05-11 05:30:10 +0000

    finished all w/o stress testing

--- week3_assignment/covering_segments.cpp
+++ week3_assignment/covering_segments.cpp
@@ -9,13 +9,55 @@ struct Segment {
   int start, end;
 };
 
+vector<Segment> bubble_fast(vector<Segment> input){
+  int n = input.size();
+  while(true){
+    int newn = 0;
+    for(int i=1; i<=n-1; i++){
+      if(input[i-1].start > input[i].start){
+        std::swap(input[i-1], input[i]);
+        newn = i;
+      }
+    }
+    n = newn;
+    if(n==0) break;
+  }
+  return input;
+}
+
 vector<int> optimal_points(vector<Segment> &segments) {
   vector<int> points;
   //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+  
+  segments = bubble_fast(segments);
+  
+  int index = 0;
+  while(true){
+    if(index >= segments.size()-1) break;
+    
+    Segment range;
+    range.start = segments[index].start;
+    range.end = segments[index].end;
+    
+    for(int i = index; i < segments.size(); ++i){
+      index = i;
+      
+      if(segments[i].start < range.end){
+        if(segments[i].start > range.start){
+          range.start = segments[i].start;
+        }
+        //segments is sorted accroding to its starts
+        
+        if(segments[i].end < range.end){
+          range.end = segments[i].end;
+        }
+      }else{
+        break;
+      }
+    }
+    points.push_back(range.end);
   }
+  
   return points;
 }
 
@@ -23,12 +65,13 @@ int main() {
   int n;
   std::cin >> n;
   vector<Segment> segments(n);
-  for (size_t i = 0; i < segments.size(); ++i) {
+  for (int i = 0; i < segments.size(); ++i) {
     std::cin >> segments[i].start >> segments[i].end;
   }
   vector<int> points = optimal_points(segments);
   std::cout << points.size() << "\n";
-  for (size_t i = 0; i < points.size(); ++i) {
+  for (int i = 0; i < points.size(); ++i) {
     std::cout << points[i] << " ";
   }
+  std::cout << std::endl;
 }
--- week3_assignment/different_summands.cpp
+++ week3_assignment/different_summands.cpp
@@ -6,6 +6,19 @@ using std::vector;
 vector<int> optimal_summands(int n) {
   vector<int> summands;
   //write your code here
+  
+  summands.push_back(0);
+  
+  while(true){
+    n = n - summands.back();
+    if(n <= 2*(summands.back()+1)){
+      summands.push_back(n);
+      break;
+    }else{
+      summands.push_back(summands.back()+1);
+    }
+  }
+  summands.erase(summands.begin());
   return summands;
 }
 
@@ -17,4 +30,5 @@ int main() {
   for (size_t i = 0; i < summands.size(); ++i) {
     std::cout << summands[i] << ' ';
   }
+  std::cout << std::endl;
 }
